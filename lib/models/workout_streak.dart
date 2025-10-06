37b3367d4b9d919ee733172616e49a22a788038a
Dia Monique <diabaylon1013@gmail.com>
2025-10-07 02:18:36 +0800

    Add files via upload

--- lib/models/workout_streak.dart
+++ lib/models/workout_streak.dart
@@ -10,4 +10,36 @@ class WorkoutStreak {
     required this.lastWorkout,
     required this.workoutDates,
   });
+
+  // Convert to Map for Firestore
+  Map<String, dynamic> toMap() {
+    return {
+      'currentStreak': currentStreak,
+      'bestStreak': bestStreak,
+      'lastWorkout': lastWorkout.toIso8601String(),
+      'workoutDates': workoutDates.map((date) => date.toIso8601String()).toList(),
+    };
+  }
+
+  // Create from Map from Firestore
+  factory WorkoutStreak.fromMap(Map<String, dynamic> map) {
+    return WorkoutStreak(
+      currentStreak: map['currentStreak'] ?? 0,
+      bestStreak: map['bestStreak'] ?? 0,
+      lastWorkout: DateTime.parse(map['lastWorkout']),
+      workoutDates: (map['workoutDates'] as List<dynamic>)
+          .map((dateString) => DateTime.parse(dateString as String))
+          .toList(),
+    );
+  }
+
+  // Create empty streak
+  factory WorkoutStreak.empty() {
+    return WorkoutStreak(
+      currentStreak: 0,
+      bestStreak: 0,
+      lastWorkout: DateTime.now().subtract(const Duration(days: 2)),
+      workoutDates: [],
+    );
+  }
 }
